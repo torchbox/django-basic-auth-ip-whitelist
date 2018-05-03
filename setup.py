db1551726c5b53eb5212448b3f95d372c426400f
Tomasz Knapik <tmkn@tmkn.uk>
2018-05-03 19:52:06 +0100

    Release 0.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
         'Hide your Django site behind basic authentication mechanism with IP '
         'whitelisting support.'
     ),
-    version='0.1a3',
+    version='0.1',
     license='BSD-2',
     packages=['baipw'],
     install_requires=[
@@ -16,4 +16,24 @@ setup(
     author='Tomasz Knapik',
     author_email='tmkn@tmkn.uk',
     url='https://github.com/tm-kn/django-basic-auth-ip-whitelist',
+    keywords=[
+        'basic',
+        'authentication',
+        'auth',
+        'ip',
+        'whitelist',
+        'whitelisting',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2',
+    ]
 )
