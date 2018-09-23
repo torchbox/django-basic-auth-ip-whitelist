a41a441f958b175f400515d7b35c6d70e9cb32c9
Tomasz Knapik <me@tmkn.org>
2018-09-23 04:47:25 +0100

    Add basic integration tests

--- baipw/tests/settings.py
+++ baipw/tests/settings.py
@@ -7,12 +7,14 @@ TESTS_PATH = dirname(abspath(__file__))
 
 SECRET_KEY = get_random_string(50)
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
-    }
-}
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
 
 TEMPLATES = [
     {
@@ -20,8 +22,25 @@ TEMPLATES = [
         'DIRS': [
             join(TESTS_PATH, 'templates'),
         ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+            ],
+        },
     },
 ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
+}
+
+ROOT_URLCONF = 'baipw.tests.urls'
 
 if django.VERSION < (1, 9):
     # This is to satisfy the django-admin check command.
